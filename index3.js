f835f9d82ce0ded454b3bf39d4dcd4094b1be8ea
dshilkret <dshilkret@gmail.com>
2020-10-13 09:31:35 -0500

    updates

--- index3.js
+++ index3.js
@@ -4,36 +4,7 @@ var url = require("url");
 var request = require("request");
 
 
-/*
- * base64.js: An extremely simple implementation of base64 encoding / decoding using node.js Buffers
- *
- * (C) 2010, Nodejitsu Inc.
- * (C) 2011, Cull TV, Inc.
- *
- */
-
-var base64 = exports;
-
-base64.encode = function(unencoded) {
-  return new Buffer(unencoded || '').toString('base64');
-};
-
-base64.decode = function(encoded) {
-  return new Buffer(encoded || '', 'base64').toString('utf8');
-};
-
-base64.urlEncode = function(unencoded) {
-  var encoded = base64.encode(unencoded);
-  return encoded.replace('+', '-').replace('/', '_').replace(/=+$/, '');
-};
-
-base64.urlDecode = function(encoded) {
-  encoded = encoded.replace('-', '+').replace('_', '/');
-  while (encoded.length % 4)
-    encoded += '=';
-  return base64.decode(encoded);
-};
-
+  
 var siteUrl = 'https://spwzb27rov2c3sm.eastus2.cloudapp.azure.com';
 var sharepointhostname = url.parse(siteUrl).hostname;
 var clientid = 'b66f7e77-de3e-45d2-ba4c-b1b6405ec214';
@@ -65,16 +36,13 @@ var actortoken = {
 }
 //get accessToken
 var accessToken = jwt.sign(actortoken, options.key, { header: rs256 });
-var outerHead = base64.urlEncode('{"typ":"JWT", "alg":"none"}');
-var outerBodyPre = {"iss":"9e9e46c4-6329-4990-a0b8-13b87b3ba56a@f094a07e-5375-4b96-a81c-f651a25f5788","nameid":"s-1-5-21-2127521184-1604012920-1887927527-2963467", "nii":"urn:office:idp:activedirectory","nbf":"1403212820", "exp":"1633881331","actortoken":accessToken};
-var outerBody = base64.urlEncode(JSON.stringify(outerBodyPre));
- var outerToken = outerHead + '.' + outerBody;
+
 request.get({
   url: `${siteUrl}/_api/web/lists`,
   rejectUnauthorized: false,
   headers: {
     'Accept': 'application/json;odata=verbose',
-    'Authorization': 'Bearer ' + outerToken  //accessToken
+    'Authorization': 'Bearer ' + accessToken
   }
 }, function (error, response, body) {
   
