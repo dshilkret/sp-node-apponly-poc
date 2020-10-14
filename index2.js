7fc8056f131d2e3284eecec2a6c679f0c7faaed0
David Shilkret <dshilkret@gmail.com>
2020-10-14 09:15:01 -0500

    updates

--- index2.js
+++ index2.js
@@ -5,6 +5,7 @@ var request = require("request");
 
 
 //shaThumbprint: 'QOCIyWwlxy8bM40Og6yzuj9vYkU'
+
 var siteUrl = 'https://spwzb27rov2c3sm.eastus2.cloudapp.azure.com';
 var sharepointhostname = url.parse(siteUrl).hostname;
 var clientid = 'b66f7e77-de3e-45d2-ba4c-b1b6405ec214';
@@ -14,6 +15,16 @@ var audience = '00000003-0000-0ff1-ce00-000000000000/' + sharepointhostname + '@
 var x5t = 'QOCIyWwlxy8bM40Og6yzuj9vYkU';
 var keyFilePath = 'C:\\DocIntegrator\\DIA\\SP OAuth-2016\\OAuth\\HighTrustOAuth.key';
 
+/*
+var siteUrl = 'https://spg4sc7skkehdns.eastus2.cloudapp.azure.com';
+var sharepointhostname = 'spg4sc7skkehdns.eastus2.cloudapp.azure.com';
+var clientid = '87d59c0e-6a2b-408b-9a6c-1f48392d303c';
+var realm = 'f094a07e-5375-4b96-a81c-f651a25f5788';
+issuerid = '9e9e46c4-6329-4990-a0b8-13b87b3ba56a'+ "@" + realm;  //9e9e46c4-6329-4990-a0b8-13b87b3ba56a
+audience = '00000003-0000-0ff1-ce00-000000000000/' + sharepointhostname + '@' + realm;
+var x5t = '9Y78CA2y1PLHqfaEBNpPf8Y3leA';
+var keyFilePath = 'C:\\DocIntegrator\\documentation\\spaddin_real.key';
+*/
 var options = {
   key: fs.readFileSync(keyFilePath)
 };
